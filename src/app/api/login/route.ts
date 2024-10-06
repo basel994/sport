8084fbcba9b052aa269eaa69177b5b9ef6397cc6
baselbalkes <baselbalkes77@gmail.com>
2024-10-06 10:00:03 +0300

    add api for checking users login

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,18 @@
+import { sql } from "@vercel/postgres";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    const {email, password} = await request.json();
+    try {
+        const userQuery = await sql `
+        SELECT * FROM users WHERE email=${email} and password=${password}
+        `; 
+        if(!userQuery.rows[0]) {
+            return NextResponse.json({message: "Invalid credentials"})
+        }
+        return NextResponse.json({message: "Login successful"});
+    } catch(error) {
+        console.log(error);
+        return NextResponse.json({error: "Error logging in"})
+    }
+}
