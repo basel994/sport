ce893bfd1288631f5f05cbc6f30a113282cd05be
baselbalkes <baselbalkes77@gmail.com>
2024-10-07 20:39:10 +0300

    test2

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,20 +6,20 @@ import { UserType } from '@/types/users/usersType';
 export function middleware(req: NextRequest) {  
     const token = req.cookies.get("token");
     if (req.nextUrl.pathname.startsWith('/dashboard')) {
-        try {
             if (token) { 
-                const decoded  = jwt.verify(token.value, process.env.JWT_SECRET!) as UserType;
-                if(decoded.role !== "admin"){
-                    return NextResponse.redirect(new URL('/hello', req.url));
+                try {
+                    const decoded  = jwt.verify(token.value, process.env.JWT_SECRET!) as UserType;
+                    if(decoded.role !== "admin"){
+                        return NextResponse.redirect(new URL('/hello', req.url));
+                    }
+                } catch (err) {  
+                    console.log(err);
+                    return NextResponse.redirect(new URL('/', req.url));  
                 }
             }
             else {
                 return NextResponse.redirect(new URL('/login', req.url));  
             }
-        } catch (err) {  
-            console.log(err);
-            return NextResponse.redirect(new URL('/', req.url));  
-        }
-        return NextResponse.next();    
-    }  
+            return NextResponse.next(); 
+    }   
 }  
