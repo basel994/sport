a31b842baa94c8ea16f045ffa50d90cb4b1bf2c2
baselbalkes <baselbalkes77@gmail.com>
2024-10-07 19:46:36 +0300

    add middleware to protect admin routes

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+// app/middleware.ts  
+import { NextRequest, NextResponse } from 'next/server';  
+import jwt from 'jsonwebtoken';  
+import { UserType } from '@/types/users/usersType';
+
+export function middleware(req: NextRequest) {  
+    if (req.nextUrl.pathname.startsWith('/dashboard')) {
+        try {
+            const token = req.cookies.get("token");
+            if (token) { 
+                const decoded  = jwt.verify(token.value, process.env.JWT_SECRET!) as UserType;
+                if(decoded.role !== "admin"){
+                    return NextResponse.redirect(new URL('/', req.url));
+                }
+            }
+            else {
+                return NextResponse.redirect(new URL('/login', req.url));  
+            }
+        } catch (err) {  
+            return NextResponse.redirect(new URL('/login', req.url));  
+        }   
+    } 
+}  
