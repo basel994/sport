4306ac393f9cf8d7e7c462931c1df8517bf3906d
baselbalkes <baselbalkes77@gmail.com>
2024-10-08 20:33:38 +0300

    add handling for user logout

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,8 +2,24 @@
 import Link from "next/link";
 import styles from "./sidebar.module.css";
 import { useUser } from "@/context/userContext";
+import { useRouter } from "next/navigation";
 export default function Sidebar() {
-    const{ user } = useUser();
+    const router = useRouter();
+    const{ user, setUser } = useUser();
+    const logoutHandle = async() => {
+        const logoutApi = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/logout`, {
+            method: "POST",
+        });
+        const logoutResponse = await logoutApi.json();
+        if(logoutResponse.error){
+            console.log(logoutResponse.error);
+        }
+        else {
+            setUser(null);
+            console.log(logoutResponse.message);
+            router.push("/");
+        }
+    }
     return(
         <div className={styles.side}>
             <h1>side navbar</h1>
