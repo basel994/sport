c213abe6ffa6f278f4a1440594f575ebb9f237cf
baselbalkes <baselbalkes77@gmail.com>
2024-10-11 15:35:52 +0300

    remove user from localStorage after logout

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -16,6 +16,7 @@ export default function Sidebar() {
         }
         else {
             setUser(null);
+            localStorage.removeItem("user");
             console.log(logoutResponse.message);
             router.push("/");
         }
